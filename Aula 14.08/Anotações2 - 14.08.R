e056ef3940fd414180c289e4a83572f0926227ae
Gabriel Peixoto <gabriel.peixoto2002@hotmail.com>
2026-09-01 13:01:00 -0300

    Adiciona anotações de aulas passadas

---
+++ Aula 14.08/Anotações2 - 14.08.R
@@ -0,0 +1,20 @@
+Acho que esse Rscript é o "Anotações" do R. 
+
+Se for isso, será bem útil. 
+
+1a observação: Usar o "Projects" é muito útil para organização. Basicamente, crio um diretório em que posso colocar tudo que preciso para um trabalho.
+Nesse caso, posso criar um Project para cada aula. 
+
+//Não sei como mudar nome de cada documento ainda kkkk mas vai indo. 
+
+Cada aula terá um pequeno teste (Laboratório do Moodle), e no final da aula, alguém será sorteado para falar sobre. 
+
+-- Comando "tecla Windows + setinha" para dividir a tela em duas janelas. Bem útil.
+
+-- Função paste cria várias variáveis com nomes parecidos, tipo aluno1, aluno2, aluno3... facilmente.
+
+-- Erro NaN significa "Not a Number".
+
+-- min, max, sum, prod, operações... tudo isso lá nas anotações.
+
+-- sort = função para colocar em ordem crescente ou decrescente.
---
+++ Aula 21.08/Anotações2 - 21.08.R
@@ -0,0 +1,13 @@
+
+
+unique() para saber quantos e quais elementos existem em um data frame
+extenso. 
+
+aggregate() formula usada para descobrir características estatísticas
+(média, moda, sd, max, min...) de uma coluna específica de um dataframe
+
+merge() usado para fundir dois dataframes em um só. é melhor usado se 
+houver uma coluna em comum entre os dois, como 'id'. Bem fácil de usar.
+
+sub() para substituir palavras por outras em dataframes. bem fácil de 
+usar e útil. 
